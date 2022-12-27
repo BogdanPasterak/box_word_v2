bb9a5ed0770000c6b0494b46ac485881ce0bb4d6
bogdanpasterak <bogdanpasterak@gmail.com>
2022-12-27 21:57:14 +0000

    first function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 
 function App() {
-  const name = "Bogdan";
+  let board = ["a x x x", "b x x x", "c x x x", "d x x x"];
+
+  const run = () => {
+    console.log("Bogdan");
+  };
 
   return (
-    <div className="App">
-      <h1 className="">{name}</h1>
+    <div>
+      <h1 className="my">{board[0]}</h1>
+      <h1 className="my">{board[1]}</h1>
+      <h1 className="my">{board[2]}</h1>
+      <h1 className="my">{board[3]}</h1>
+
+      <button onClick={run}>Click</button>
     </div>
   );
 }
