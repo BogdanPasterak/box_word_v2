9fd60d473e29b92bae7b6658744dc4f961991f3c
bogdanpasterak <bogdanpasterak@gmail.com>
2022-12-28 12:05:46 +0000

    add function in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,37 @@
 import "./App.css";
 
+class BoardObj {
+  fields;
+  space;
+
+  constructor(s = null) {
+    if (s) this.fields = [...s];
+    else this.fields = [..."xxxxxxxxxxxxxxx "];
+    this.space = 15;
+  }
+}
+
+class BoardView {
+  b;
+  constructor(bo) {
+    let f = bo.fields;
+    this.b = [];
+    this.b.push([f[0], f[1], f[2], f[3]].join(" "));
+    this.b.push([f[4], f[5], f[6], f[7]].join(" "));
+    this.b.push([f[8], f[9], f[10], f[11]].join(" "));
+    this.b.push([f[12], f[13], f[14], f[15]].join(" "));
+  }
+}
+
 function App() {
   let board = ["a x x x", "b x x x", "c x x x", "d x x x"];
 
   const run = () => {
     console.log("Bogdan");
+    let obj = new BoardObj();
+    console.log(obj.fields);
+    let view = new BoardView(obj);
+    console.log(view);
   };
 
   return (
