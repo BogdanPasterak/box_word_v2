080fac15fb4ae42a36e8077d2080d45d8b0c0c4e
bogdanpasterak <bogdanpasterak@gmail.com>
2022-12-30 13:43:32 +0000

    add backgroun image

--- frontend/public/myscript.js
+++ frontend/public/myscript.js
@@ -1,15 +1,29 @@
-console.log("Bogdan");
-window.onload = () => {
-  console.log("on load");
-};
-
+// Events call to setting the layout and scaling elements
+window.onload = () => sizeFitting();
 window.onresize = (e) => sizeFitting();
+window.onorientationchange = (e) => sizeFitting();
 
 function sizeFitting() {
+  // div with 1% of view border (css setting size)
   let div_in = document.getElementById("div-in");
-  if (div_in.clientHeight > div_in.clientWidth) {
-    console.log("wyzszy");
+  let w = div_in.clientWidth;
+  let h = div_in.clientHeight;
+
+  if (h > w * 1.2) {
+    // vertical position
+    if (h > w * 1.6) {
+      console.log("ver 16:9");
+    } else {
+      console.log("ver 3:2");
+    }
   } else {
-    console.log("szerszy");
+    // horizontal position
+    if (w * 0.8 < h) {
+      console.log("square");
+    } else if (w * 0.6 < h) {
+      console.log("hor 3:2");
+    } else {
+      console.log("hor 16:9");
+    }
   }
 }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import bg from "./image/bg.jpg";
+
 function App() {
   return (
-    <div className="procent" id="div-in">
+    <div
+      className="procent"
+      id="div-in"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       <div>
         <img src="./favicon.ico" alt="" />
       </div>
