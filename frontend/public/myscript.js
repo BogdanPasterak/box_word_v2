6c48da4c0b50127364c3c8ecf2998d2a25a87d12
bogdanpasterak <bogdanpasterak@gmail.com>
2023-01-01 15:09:29 +0000

    all size

--- frontend/public/myscript.js
+++ frontend/public/myscript.js
@@ -32,9 +32,12 @@ function sizeFitting() {
   temp = Math.floor((h - w - title.clientHeight - footer.clientHeight) / 2);
   buttons.style.height = info.style.height = temp + "px";
   // rest parameters
+  title.style.order = "1";
   buttons.style.order = "2";
   info.style.order = "3";
-  board.style.position = "";
+  board.style.order = "4";
+  footer.style.order = "5";
+  board.style.position = footer.style.position = "";
 
   if (h > w * 1.35) {
     // vertical position
@@ -45,11 +48,11 @@ function sizeFitting() {
     }
   } else {
     // horizontal position
-    if (w < h) {
+    if (w < h * 1.2) {
       // console.log("square");
       buttons.style.order = "3";
       info.style.order = "2";
-      box = w * 0.8;
+      box = w * (w < h ? 0.8 : 0.65);
       buttons.style.width = w - box + "px";
       buttons.style.height =
         board.style.width =
@@ -61,14 +64,34 @@ function sizeFitting() {
       board.style.top = temp + title.clientHeight + h * 0.01 + "px";
       info.style.height = temp + "px";
     } else if (w * 0.6 < h) {
-      console.log("hor 3:2");
-      title.style.width = "50%";
+      // console.log("hor 3:2");
+      info.style.order = "2";
+      buttons.style.order = "3";
+      box = h - title.clientHeight - footer.clientHeight;
+      info.style.width = buttons.style.width = w - box + "px";
+      info.style.height = buttons.style.height =
+        Math.floor((h - title.clientHeight) / 2) + "px";
+      board.style.width = board.style.height = footer.style.width = box + "px";
+      footer.style.position = board.style.position = "absolute";
+      footer.style.left = board.style.left = w - box + w * 0.01 + "px";
+      temp = title.clientHeight + h * 0.01;
+      board.style.top = temp + "px";
+      footer.style.top = temp + box + "px";
     } else {
-      console.log("hor 16:9");
-      board.style.width = h + "px";
-      board.style.height = h + "px";
-      title.style.width = w - h + "px";
-      footer.style.width = w - h + "px";
+      // console.log("hor 16:9");
+      info.style.order = "2";
+      buttons.style.order = "3";
+      footer.style.order = "4";
+      board.style.order = "5";
+      box = h;
+      board.style.width = board.style.height = box + "px";
+      title.style.width =
+        info.style.width =
+        buttons.style.width =
+        footer.style.width =
+          w - box + "px";
+      temp = Math.floor((h - title.clientHeight - footer.clientHeight) / 2);
+      info.style.height = buttons.style.height = temp + "px";
     }
   }
 }
