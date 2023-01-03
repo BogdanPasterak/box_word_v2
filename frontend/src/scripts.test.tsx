55ea100e5a11bfd7cb25d5cbc1119c95a7fca7f8
bogdanpasterak <bogdanpasterak@gmail.com>
2023-01-03 17:41:40 +0000

    test test

--- frontend/src/scripts.test.tsx
+++ frontend/src/scripts.test.tsx
@@ -1,5 +1,63 @@
-import { generateStub } from "./scripts";
+import { render } from "@testing-library/react";
+import { generateStub, winTest } from "./scripts";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(true).toBe(true);
+describe("Testing generateStub", () => {
+  const abcd = ["A", "B", "C", "D"];
+  let answer = generateStub();
+
+  test(`should be string "${answer}"`, () => {
+    expect(typeof answer).toBe("string");
+  });
+
+  test("length should be 16", () => {
+    expect(answer.length).toBe(16);
+  });
+
+  test("last one should be [space]", () => {
+    expect(answer[15]).toBe(" ");
+  });
+
+  // test each letter
+  abcd.forEach((letter) => {
+    test(`should contain one [${letter}]`, () => {
+      // contain first
+      expect(answer.includes(letter)).toBe(true);
+      // no next
+      let indexNext = answer.indexOf(letter) + 1;
+      expect(answer.includes(letter, indexNext)).toBe(false);
+    });
+  });
+
+  for (let i = 0; i < 15; i++) {
+    if (!abcd.includes(answer[i]))
+      test(`at index (${i}) should be [*]`, () => {
+        expect(answer[i]).toBe("*");
+      });
+  }
+});
+
+describe("Testing winTest", () => {
+  const win = [
+    "ABCD*********** ",
+    "A***B***C***D** ",
+    "A****B****C*** D",
+    "***A** B***C***D",
+    "***A**B**C**D** ",
+    "***** ******ABCD",
+    "***D**C**B**A** ",
+    "****ABCD* ******",
+    "********ABCD*** ",
+    "*A***B***C***D* ",
+    "**A***B***C***D ",
+  ];
+  const word = "ABCD";
+  let answer: boolean;
+
+  // test win set
+  win.forEach((w) => {
+    answer = winTest(w, word);
+    test(`should be win "${w}"`, () => {
+      expect(answer).toBeTruthy();
+    });
+  });
 });
--- frontend/src/scripts.tsx
+++ frontend/src/scripts.tsx
@@ -9,12 +9,53 @@ export function generateStub(): string {
 
   //random position of 4 letters
   ["A", "B", "C", "D"].forEach((e) => {
-    do index = Math.floor(Math.random() * 15);
-    while (!(temp[index] === "*"));
+    do {
+      index = Math.floor(Math.random() * 15);
+    } while (!(temp[index] === "*"));
 
     temp[index] = e;
   });
 
   // connect to string
   return temp.join("");
+}
+
+// test if board match word
+export function winTest(b: string, word: string): boolean {
+  // first position top left corner - 3 posible
+  if (b[0] === word[0]) {
+    if (
+      (b[1] === word[1] && b[2] === word[2] && b[3] === word[3]) ||
+      (b[4] === word[1] && b[8] === word[2] && b[12] === word[3]) ||
+      (b[5] === word[1] && b[10] === word[2] && b[15] === word[3])
+    )
+      return true;
+  } else if (b[3] === word[0]) {
+    // top right corner - 2 posible down and cross
+    if (
+      (b[7] === word[1] && b[11] === word[2] && b[15] === word[3]) ||
+      (b[6] === word[1] && b[9] === word[2] && b[12] === word[3])
+    )
+      return true;
+  } else if (b[12] === word[0]) {
+    // bottom left corner - 2 posible right and cross
+    if (
+      (b[13] === word[1] && b[14] === word[2] && b[15] === word[3]) ||
+      (b[9] === word[1] && b[6] === word[2] && b[3] === word[3])
+    )
+      return true;
+  } else if (b[4] === word[0]) {
+    // 2-nd horizontal
+    if (b[5] === word[1] && b[6] === word[2] && b[7] === word[3]) return true;
+  } else if (b[8] === word[0]) {
+    // 3-th horizontal
+    if (b[9] === word[1] && b[10] === word[2] && b[11] === word[3]) return true;
+  } else if (b[1] === word[0]) {
+    // 2-nd vertical
+    if (b[5] === word[1] && b[9] === word[2] && b[13] === word[3]) return true;
+  } else if (b[2] === word[0]) {
+    // 3-th vertical
+    if (b[6] === word[1] && b[10] === word[2] && b[14] === word[3]) return true;
+  }
+  return false;
 }
