9e87f3db1aace1fe7da04d23a338a46d9f6d9061
bogdanpasterak <bogdanpasterak@gmail.com>
2023-01-18 18:55:59 +0000

    refactor board model (prototype)

--- frontend/src/models/board.tsx
+++ frontend/src/models/board.tsx
@@ -11,64 +11,77 @@ export class BoardObj implements Board {
   from: number[];
   pos: number;
 
-  constructor(board: string, word: string) {
-    this.board = board;
-    this.word = word;
-    this.from = [];
-    this.pos = 15;
+  constructor(obj: BoardObj);
+  constructor(board: string, word: string);
+
+  constructor(...arr: any[]) {
+    if (arr[0] instanceof BoardObj) {
+      this.board = arr[0].board;
+      this.word = arr[0].word;
+      this.from = [...arr[0].from];
+      this.pos = arr[0].pos;
+    } else {
+      this.board = arr[0];
+      this.word = arr[1];
+      this.from = [];
+      this.pos = 15;
+    }
   }
+}
 
-  copy(): BoardObj {
-    let c = new BoardObj(this.board, this.word);
-    c.pos = this.pos;
-    c.from = [...this.from];
+export interface BoardObj {
+  copy: () => BoardObj;
+  move: (p: number) => BoardObj;
+  back: () => BoardObj;
+  toString: () => string;
+}
 
-    return c;
-  }
+BoardObj.prototype.copy = function () {
+  return new BoardObj(this);
+};
 
-  move(p: number): BoardObj {
-    let swap = this.board[p];
+BoardObj.prototype.move = function (p: number) {
+  let swap = this.board[p];
+
+  // swap two element [space and element at index p]
+  this.board = this.board
+    .split("")
+    .map((e, i) => (i === this.pos ? swap : i === p ? " " : e))
+    .join("");
+
+  // store move
+  this.from.push(this.pos);
+  // new positoion space
+  this.pos = p;
+  return this;
+};
+
+BoardObj.prototype.back = function () {
+  if (this.from.length) {
+    let old = this.from.pop() || -1;
+    let swap = this.board[old];
 
     // swap two element [space and element at index p]
     this.board = this.board
       .split("")
-      .map((e, i) => (i === this.pos ? swap : i === p ? " " : e))
+      .map((e, i) => (i === old ? " " : i === this.pos ? swap : e))
       .join("");
 
-    // store move
-    this.from.push(this.pos);
     // new positoion space
-    this.pos = p;
-    return this;
+    this.pos = old;
   }
+  return this;
+};
 
-  back() {
-    if (this.from.length) {
-      let old = this.from.pop() || -1;
-      let swap = this.board[old];
+BoardObj.prototype.toString = function () {
+  let s = this.board.substring(0, 4).split("").join(" ");
+  s += `\tword  - ${this.word}\n`;
+  s += this.board.substring(4, 8).split("").join(" ");
+  s += `\tspace - ${this.pos}\n`;
+  s += this.board.substring(8, 12).split("").join(" ");
+  s += `\tlevel - ${this.from.length}\n`;
+  s += this.board.substring(12).split("").join(" ");
+  s += `\tfrom  - [${this.from.toString()}]`;
 
-      // swap two element [space and element at index p]
-      this.board = this.board
-        .split("")
-        .map((e, i) => (i === old ? " " : i === this.pos ? swap : e))
-        .join("");
-
-      // new positoion space
-      this.pos = old;
-    }
-    return this;
-  }
-
-  public toString = (): string => {
-    let s = this.board.substring(0, 4).split("").join(" ");
-    s += `\tword  - ${this.word}\n`;
-    s += this.board.substring(4, 8).split("").join(" ");
-    s += `\tspace - ${this.pos}\n`;
-    s += this.board.substring(8, 12).split("").join(" ");
-    s += `\tlevel - ${this.from.length}\n`;
-    s += this.board.substring(12).split("").join(" ");
-    s += `\tfrom  - [${this.from.toString()}]`;
-
-    return s;
-  };
-}
+  return s;
+};
