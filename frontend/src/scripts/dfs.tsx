f03e0818ca1ad686eadee2cc0e4b9738d42b7c2f
BogdanPasterak <L00157106@student.lyit.ie>
2023-02-07 22:09:53 +0000

    19 c

--- frontend/src/scripts/dfs.tsx
+++ frontend/src/scripts/dfs.tsx
@@ -116,7 +116,7 @@ export function openFile2() {
   const setsUsed = arr.concat(arr17).concat(arr18).sort();
   console.log("used =", setsUsed.length);
 
-  for (let a = 5; a < 8; a++) {
+  for (let a = 8; a < 11; a++) {
     for (let b = 0; b < 15; b++) {
       if (b === a) continue;
       now = Math.floor((Date.now() - start) / 60000);
