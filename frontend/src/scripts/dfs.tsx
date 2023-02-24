00c126dfdaea969ebeff5ef690c1f0becb420ae2
BogdanPasterak <L00157106@student.lyit.ie>
2023-02-24 10:11:06 +0000

    22_9

--- frontend/src/scripts/dfs.tsx
+++ frontend/src/scripts/dfs.tsx
@@ -117,7 +117,7 @@ export function openFile2() {
   const level = 22;
   // console.log(`========= LEVEL ${level} ============`);
   const abc = "abcdefghijklmno";
-  const index = 6;
+  const index = 10;
 
   // const part = allParts[index];
   console.log(`========= LEVEL ${level} ${abc[index]} ============`);
